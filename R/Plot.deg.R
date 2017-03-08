7bb09d11f7e53c9ce9bab9ab0f4909699e562e9e
menggf <menggf@gmail.com>
2017-03-08 13:21:06 +0800

    0.0.5

---
+++ R/Plot.deg.R
@@ -0,0 +1,363 @@
+#' Plot the DEGs before or after cross-validation
+#'
+#' Plot the binary differential expression matrix transformed by \code{\link{bi.deg}}
+#'
+#' @import ComplexHeatmap
+#'
+#' @name Plot.deg
+#' @param input a 'deg' object returned by \code{\link{bi.deg}}
+#' @param ann a data.frame for the patient annotation
+#' @param col.order the order of column in heatmap
+#' @param show.genes the gene ids to plot
+#' @param max.n the maximum number of genes to plot
+#' @param up.col the color for up-regulated genes
+#' @param down.col the color for down-regulated genes
+#'
+#' @author Guofeng Meng
+#'
+#' @references
+#' Gu Z, Eils R and Schlesner M (2016). “Complex heatmaps reveal patterns and correlations in multidimensional genomic data.” Bioinformatics.
+#'
+#' @details This function applied the function of oncoPrint from `ComplexHeatmap` to dispaly ownership of the DEGs. The output is a heatmap plots where the genes with maximum observations are showed.
+#'
+#' @return A heatmap plot
+#'
+#' @examples
+#' \dontrun{
+#' Plot(deg,er.ann, max.n=15)
+#' Plot(res.deg, er.ann, max.n=15)
+#' Plot(res.deg, ann=er.ann, show.genes=c("ESR1","FOXA1","GATA3"))
+#' Plot(res.deg, ann=er.ann, up.col="#008000", down.col="#CD5B45")
+#' }
+#'
+#' @export
+
+Plot.deg<-function(input, ann=NULL, col.order=NULL, show.genes=NULL, max.n=30, up.col="#008000", down.col="#CD5B45"){
+    if(!is.null(ann) & !is(ann,"data.frame"))
+        stop("Error: ann: should be data.frame!")
+		input=as.matrix(input)
+		ges=row.names(input)
+		pas=colnames(input);
+		if(!all(unique(as.vector(input))%in%c(1,-1,0)))
+			stop("Error: deg: valid values are  1, -1 and 0")
+		if(is.null(show.genes)){
+			aa=sort(apply(input, 1, function(x) length(x[x!=0])),decreasing=T);
+			show.genes=names(aa[1:min(max.n, dim(input)[1])]);
+			if(aa[length(show.genes)]==0)
+				show.genes=names(aa[aa!=0]);
+		}
+		else{
+			all.ids=row.names(input);
+			show.genes=show.genes[show.genes%in%all.ids];
+		}
+		if(length(show.genes)==0)
+			stop("Error: show.genes: cannot recognize the ids")
+		mat.deg=t(sapply(show.genes, function(x) {
+			y=input[x,];
+			rr=rep("",length(y));
+			rr[y == 1]="Up";
+			rr[y == -1]="Down";
+			return(rr);
+		}))
+		row.names(mat.deg)<-show.genes;
+		colnames(mat.deg)<- colnames(input);
+
+		ha=NULL;
+	if(!is.null(ann)){
+		has.pas=row.names(ann);
+		if(length(which(has.pas%in%pas)) < 0.6 *length(pas))
+			warnings("Warning: ann: Too few patients has annotation");
+		if(length(which(has.pas%in%pas)) < 0.3 *length(pas))
+			stop("Error: ann: Too few patients has annotation");
+		all.ann = unique(as.vector(as.matrix(ann)))
+		all.ann=all.ann[!is.na(all.ann)]
+		cl=rainbow(length(all.ann));
+		names(cl)<-all.ann;
+		col.list=lapply(names(ann), function(x) {	return(cl)	});
+		names(col.list)<-names(ann)
+		if(dim(ann)[2]==1){
+		  new.ann=as.data.frame(ann[pas,])
+		  row.names(new.ann)<-pas;
+		  names(new.ann)<-names(ann);
+		}else{
+		  new.ann=ann[pas,]
+		}
+		ha=HeatmapAnnotation(df=new.ann, annotation_height=0.2, name=names(ann), col=col.list)
+	}
+	alter_fun = list(
+	   	background = function(x, y, w, h) {
+	       	grid.rect(x, y, w-unit(0.5, "mm"), h-unit(0.5, "mm"), gp = gpar(fill = "#CCCCCC", col = NA))
+	   	},
+	   	Up = function(x, y, w, h) {
+	       	grid.rect(x, y, w-unit(0.5, "mm"), h*0.33, gp = gpar(fill = up.col, col = NA))
+	   	},
+	   	Down = function(x, y, w, h) {
+	       	grid.rect(x, y, w-unit(0.5, "mm"), h*0.33, gp = gpar(fill = down.col, col = NA))
+	   	}
+	)
+	col = c("Up" = up.col, "Down" = down.col)
+	if(!is.null(ha)){
+		if(is.null(col.order)){
+			oncoPrint(mat.deg, get_type = function(x) strsplit(x, ";")[[1]], bottom_annotation= ha, alter_fun = alter_fun, col = col, column_title = "", heatmap_legend_param = list(title = "DEG"))
+		}
+		else{
+			oncoPrint(mat.deg, get_type = function(x) strsplit(x, ";")[[1]], column_order=col.order, bottom_annotation= ha, alter_fun = alter_fun, col = col, column_title = "", heatmap_legend_param = list(title = "DEG"))
+		}
+	}
+	else{
+		if(is.null(col.order)){
+			oncoPrint(mat.deg, get_type = function(x) strsplit(x, ";")[[1]],  alter_fun = alter_fun, col = col, column_title = "", heatmap_legend_param = list(title = "DEG"))
+		}
+		else{
+			oncoPrint(mat.deg, get_type = function(x) strsplit(x, ";")[[1]], column_order=col.order, alter_fun = alter_fun, col = col, column_title = "", heatmap_legend_param = list(title = "DEG"))
+		}
+	}
+}
+
+#' Plot the DEGs before or after cross-validation
+#'
+#' Plot the cross-validated DEGs predicted by \code{\link{deg.specific}}.
+#'
+#' @import ComplexHeatmap
+#'
+#' @name Plot.deg.specific
+#' @param input a 'deg.specific' object returned by \code{\link{deg.specific}}
+#' @param ann a data.frame for the patient annotation
+#' @param col.order the order of column in heatmap
+#' @param show.genes the gene ids to plot
+#' @param max.n the maximum number of genes to plot
+#' @param up.col the color for up-regulated genes
+#' @param down.col the color for down-regulated genes
+#'
+#' @author Guofeng Meng
+#'
+#' @references
+#' Gu Z, Eils R and Schlesner M (2016). “Complex heatmaps reveal patterns and correlations in multidimensional genomic data.” Bioinformatics.
+#'
+#' @details This function applied the function of oncoPrint from `ComplexHeatmap` to dispaly ownership of the DEGs. The output is a heatmap plots where the genes with maximum observations are showed.
+#'
+#' @return A heatmap plot
+#'
+#' @examples
+#' \dontrun{
+#' Plot(deg,er.ann, max.n=15)
+#' Plot(res.deg, er.ann, max.n=15)
+#' Plot(res.deg, ann=er.ann, show.genes=c("ESR1","FOXA1","GATA3"))
+#' Plot(res.deg, ann=er.ann, up.col="#008000", down.col="#CD5B45")
+#' }
+#'
+#' @export
+
+Plot.deg.specific<-function(input, ann=NULL, col.order=NULL, show.genes=NULL, max.n=30, up.col="#008000", down.col="#CD5B45"){
+    if(!is.null(ann) & !is(ann,"data.frame"))
+      stop("Error: ann: should be data.frame!")
+		ges=input[["decd.input"]][["genes"]]
+		pas=input[["decd.input"]][["patients"]]
+		dmx=input[["decd.input"]][["deg"]]
+		dmx2=matrix(ncol=length(pas),nrow=length(ges))
+		dmx2[,]=0
+		row.names(dmx2)<-ges
+		colnames(dmx2)<-pas
+		pa.ids=names(input);
+		pa.ids=pa.ids[pa.ids %in% pas ]
+		for(pa in pa.ids){
+			temp=input[[pa]][["genes"]];
+			wh=which(ges%in%temp);
+			dmx2[wh, pa]=dmx[wh,pa];
+		}
+
+		if(is.null(show.genes)){
+			aa=sort(apply(dmx2, 1, function(x) length(x[x!=0])),decreasing=T);
+			show.genes=names(aa[1:min(max.n, dim(dmx2)[1])]);
+			if(aa[length(show.genes)]==0)
+				show.genes=names(aa[aa!=0]);
+		}
+		else{
+			show.genes=show.genes[show.genes%in%ges];
+			if(length(show.genes)==0)
+				stop("Error: show.genes: cannot recognize the ids")
+		}
+		mat.deg=t(sapply(show.genes, function(x) {
+			y=dmx2[x,];
+			rr=rep("",length(y));
+			rr[y == 1]="Up";
+			rr[y == -1]="Down";
+			return(rr);
+		}))
+		row.names(mat.deg)<-show.genes;
+		colnames(mat.deg)<-pas;
+		ha=NULL;
+	if(!is.null(ann)){
+		has.pas=row.names(ann);
+		if(length(which(has.pas%in%pas)) < 0.6 *length(pas))
+			Warnings("Warning: ann: Too few patients has annotation");
+		if(length(which(has.pas%in%pas)) < 0.3 *length(pas))
+			stop("Error: ann: Too few patients has annotation");
+		all.ann = unique(as.vector(as.matrix(ann)))
+		all.ann=all.ann[!is.na(all.ann)]
+		cl=rainbow(length(all.ann));
+		names(cl)<-all.ann;
+		col.list=lapply(names(ann), function(x) {	return(cl)	});
+		names(col.list)<-names(ann)
+
+		if(dim(ann)[2]==1){
+		  new.ann=as.data.frame(ann[pas,])
+		  row.names(new.ann)<-pas;
+		  names(new.ann)<-names(ann);
+		}else{
+		  new.ann=ann[pas,]
+		}
+		ha=HeatmapAnnotation(df=new.ann, annotation_height=0.2, name=names(ann), col=col.list)
+	}
+		alter_fun = list(
+	    	background = function(x, y, w, h) {
+	        	grid.rect(x, y, w-unit(0.5, "mm"), h-unit(0.5, "mm"), gp = gpar(fill = "#CCCCCC", col = NA))
+	    	},
+	    	Up = function(x, y, w, h) {
+	        	grid.rect(x, y, w-unit(0.5, "mm"), h*0.33, gp = gpar(fill = up.col, col = NA))
+	    	},
+	    	Down = function(x, y, w, h) {
+	        	grid.rect(x, y, w-unit(0.5, "mm"), h*0.33, gp = gpar(fill = down.col, col = NA))
+	    	}
+		)
+		col = c("Up" = up.col, "Down" = down.col)
+		if(!is.null(ha)){
+			if(is.null(col.order)){
+				oncoPrint(mat.deg, get_type = function(x) strsplit(x, ";")[[1]], bottom_annotation= ha, alter_fun = alter_fun, col = col, column_title = "", heatmap_legend_param = list(title = "DEG"))
+			}
+			else{
+				oncoPrint(mat.deg, get_type = function(x) strsplit(x, ";")[[1]], column_order=col.order, bottom_annotation= ha, alter_fun = alter_fun, col = col, column_title = "", heatmap_legend_param = list(title = "DEG"))
+			}
+		}
+		else{
+			if(is.null(col.order)){
+				oncoPrint(mat.deg, get_type = function(x) strsplit(x, ";")[[1]],  alter_fun = alter_fun, col = col, column_title = "", heatmap_legend_param = list(title = "DEG"))
+			}
+			else{
+				oncoPrint(mat.deg, get_type = function(x) strsplit(x, ";")[[1]], column_order=col.order, alter_fun = alter_fun, col = col, column_title = "", heatmap_legend_param = list(title = "DEG"))
+			}
+		}
+}
+#' Plot the DEGs before or after cross-validation
+#'
+#' Plot the cross-validated DEGs predicted by \code{\link{deg.specific}}.
+#'
+#' @import ComplexHeatmap
+#'
+#' @name Plot.deg.specific.test
+#' @param input a 'deg.specific' object returned by \code{\link{deg.specific}}
+#' @param ann a data.frame for the patient annotation
+#' @param col.order the order of column in heatmap
+#' @param show.genes the gene ids to plot
+#' @param max.n the maximum number of genes to plot
+#' @param up.col the color for up-regulated genes
+#' @param down.col the color for down-regulated genes
+#'
+#' @author Guofeng Meng
+#'
+#' @references
+#' Gu Z, Eils R and Schlesner M (2016). “Complex heatmaps reveal patterns and correlations in multidimensional genomic data.” Bioinformatics.
+#'
+#' @details This function applied the function of oncoPrint from `ComplexHeatmap` to dispaly ownership of the DEGs. The output is a heatmap plots where the genes with maximum observations are showed.
+#'
+#' @return A heatmap plot
+#'
+#' @examples
+#' \dontrun{
+#' Plot(deg,er.ann, max.n=15)
+#' Plot(res.deg, er.ann, max.n=15)
+#' Plot(res.deg, ann=er.ann, show.genes=c("ESR1","FOXA1","GATA3"))
+#' Plot(res.deg, ann=er.ann, up.col="#008000", down.col="#CD5B45")
+#' }
+#'
+#' @export
+Plot.deg.specific.test<-function(input, ann=NULL, col.order=NULL, show.genes=NULL, max.n=30, up.col="#008000", down.col="#CD5B45"){
+    	if(!is.null(ann) & !is(ann,"data.frame"))
+     		 stop("Error: ann: should be data.frame!")
+		ges=input[["decd.input"]][["genes"]]
+		pas=input[["decd.input"]][["patients"]]
+		dmx=input[["decd.input"]][["deg"]][ges, pas]
+		dmx2=matrix(ncol=length(pas),nrow=length(ges))
+		dmx2[,]=0
+		row.names(dmx2)<-ges;
+		colnames(dmx2)<-pas;
+		pa.ids=names(input);
+		pa.ids=pa.ids[pa.ids%in%pas];
+		for(pa in pa.ids){
+			temp=input[[pa]]$genes;
+			wh=which(ges%in%temp);
+			dmx2[wh, pa]=dmx[wh,pa];
+		}
+
+		if(is.null(show.genes)){
+			aa=sort(apply(dmx2, 1, function(x) length(x[x!=0])),decreasing=T);
+			show.genes=names(aa[1:min(max.n, dim(dmx2)[1])]);
+			if(aa[length(show.genes)]==0)
+				show.genes=names(aa[aa!=0]);
+		}
+		else{
+			show.genes=show.genes[show.genes%in%ges];
+			if(length(show.genes)==0)
+				stop("Error: show.genes: cannot recognize the gene IDs")
+		}
+		mat.deg=t(sapply(show.genes, function(x) {
+			y=dmx2[x,];
+			rr=rep("",length(y));
+			rr[y == 1]="Up";
+			rr[y == -1]="Down";
+			return(rr);
+		}))
+		row.names(mat.deg)<-show.genes;
+		colnames(mat.deg)<-pas;
+		ha=NULL;
+	if(!is.null(ann)){
+		has.pas=row.names(ann);
+		if(length(which(has.pas%in%pas)) < 0.6 *length(pas))
+			Warnings("Warning: ann: Too few patients has annotation");
+		if(length(which(has.pas%in%pas)) < 0.3 *length(pas))
+			stop("Error: ann: Too few patients has annotation");
+		all.ann = unique(as.vector(as.matrix(ann)))
+		all.ann=all.ann[!is.na(all.ann)]
+		cl=rainbow(length(all.ann));
+		names(cl)<-all.ann;
+		col.list=lapply(names(ann), function(x) {	return(cl)	});
+		names(col.list)<-names(ann)
+
+		if(dim(ann)[2]==1){
+		  new.ann=as.data.frame(ann[pas,])
+		  row.names(new.ann)<-pas;
+		  names(new.ann)<-names(ann);
+		}else{
+		  new.ann=ann[pas,]
+		}
+		ha=HeatmapAnnotation(df=new.ann, annotation_height=0.2, name=names(ann), col=col.list)
+	}
+		alter_fun = list(
+	    	background = function(x, y, w, h) {
+	        	grid.rect(x, y, w-unit(0.5, "mm"), h-unit(0.5, "mm"), gp = gpar(fill = "#CCCCCC", col = NA))
+	    	},
+	    	Up = function(x, y, w, h) {
+	        	grid.rect(x, y, w-unit(0.5, "mm"), h*0.33, gp = gpar(fill = up.col, col = NA))
+	    	},
+	    	Down = function(x, y, w, h) {
+	        	grid.rect(x, y, w-unit(0.5, "mm"), h*0.33, gp = gpar(fill = down.col, col = NA))
+	    	}
+		)
+		col = c("Up" = up.col, "Down" = down.col)
+		if(!is.null(ha)){
+			if(is.null(col.order)){
+				oncoPrint(mat.deg, get_type = function(x) strsplit(x, ";")[[1]], bottom_annotation= ha, alter_fun = alter_fun, col = col, column_title = "", heatmap_legend_param = list(title = "DEG"))
+			}
+			else{
+				oncoPrint(mat.deg, get_type = function(x) strsplit(x, ";")[[1]], column_order=col.order, bottom_annotation= ha, alter_fun = alter_fun, col = col, column_title = "", heatmap_legend_param = list(title = "DEG"))
+			}
+		}
+		else{
+			if(is.null(col.order)){
+				oncoPrint(mat.deg, get_type = function(x) strsplit(x, ";")[[1]],  alter_fun = alter_fun, col = col, column_title = "", heatmap_legend_param = list(title = "DEG"))
+			}
+			else{
+				oncoPrint(mat.deg, get_type = function(x) strsplit(x, ";")[[1]], column_order=col.order, alter_fun = alter_fun, col = col, column_title = "", heatmap_legend_param = list(title = "DEG"))
+			}
+		}
+}
---
+++ R/bi.deg.R
@@ -0,0 +1,128 @@
+#' Transformation an expression matrix to binary differential expression matrix
+#'
+#' Transform the RNA-seq counts or normalized expression matrix into binary differential expression matrix of -1, 0 and 1, which indicates the down-regulation, no change and up-regulation.
+#'
+#' @name bi.deg
+#' @param exp a matrix or data frame for expression data. The expression value can be counts or normalized expression data
+#' @param cl a vector of 0 and 1. It has equal length with the column number of exp. 1 indicates the corresponding samples are patients and 0 is control or normal
+#' @param method defines the methods applied for DE analysis. The possible value is "edger", "deseq2", "normalized". "edger" or "deseq2" is used for RNA-seq count data; "normalized" is used for normalized RNA-seq or microarray data
+#' @param cutoff the p-value cutoff for DEGs
+#' @param cores the thread number
+#'
+#' @author Guofeng Meng
+#' @references
+#'
+#' @importFrom edgeR calcNormFactors estimateDisp getDispersion equalizeLibSizes
+#' @importFrom DESeq2 DESeqDataSetFromMatrix estimateSizeFactors estimateDispersions dispersions varianceStabilizingTransformation
+#' @import parallel
+#'
+#' @details For each sample in "exp", "cl" defines the patients and normal. The normal samples are used to construct the expression references with negative binomial distribution (e.g. method="edger" or method="deseq2") or a normal distribution (method="normalized").
+#'
+#'
+#' When counts data are used, the DEG analysis is performed using the functions implemented by `DESeq2` or `edgeR`. The dispersion and mu values are estimated.
+#'
+#' @return A deg class object with value of 1, 0 and -1.
+#'
+#' @examples
+#' \dontrun{
+#' deg <- bi.deg(exp,cl=cl, method="edger", cutoff=0.05) # exp is the RNA-seq counts matrix
+#' }
+#' @export
+
+bi.deg<-function(exp, cl, method=c("edger","deseq2","normalized")[1], cutoff=0.05, cores=1 ){
+	if (!is(exp, "matrix") & !is(exp, "data.frame"))
+		stop("Error: exp: should be matrix or data.frame");
+	exp=as.matrix(exp)
+	if(method!="edger" & method != "deseq2" & method!="normalized")
+		stop("Error:method: must be edger, deseq2 or normalized data. method is not recognized");
+	if(dim(exp)[2]!=length(cl))
+		stop("Error:cl: has the wrong numbers!");
+	wh.ct=which(cl==0);
+	if(length(wh.ct)==0)
+		stop("Error: No control sample is found or control samples are not set as 0!");
+	if(length(wh.ct)<=2)
+		stop("Error: No enough control sample is used!");
+	if(length(wh.ct)<10)
+		print(paste("Warning: only ",length(wh.ct)," control samples are used!",sep=""));
+	wh.pa=which(cl==1);
+	if(length(wh.pa)==0)
+		stop("Error: No disease/treated sample is found or samples are not set as 1!");
+	genes=row.names(exp);
+	pas=colnames(exp[,wh.pa]);
+	n.ct=length(wh.ct);
+	if(method=="edger"){
+		y=DGEList(counts=exp[, wh.ct]);
+		y <- calcNormFactors(y)
+		y <- estimateDisp(y)
+		disp=as.vector(getDispersion(y))
+		mycutoff=rep(cutoff, length(disp));
+		mycutoff[disp > quantile(disp, probs=0.97)]=cutoff+0.05
+		rm(y);
+		deg.lst=mclapply(wh.pa, function(x){
+				z=DGEList(counts=exp[, c(wh.ct, x)]);
+				z2=equalizeLibSizes(z, dispersion=disp)$pseudo.counts;
+				p=pnbinom(z2[,n.ct+1], size=1/disp, mu=rowSums(z2[,1:n.ct])/n.ct, lower.tail = F)
+				bi=sapply(1:length(p), function(w){
+						if(p[w] <= mycutoff[w] )
+							return(1)
+						if(1- p[w] <= mycutoff[w] )
+							return(-1)
+						return(0);
+					});
+				return(bi);
+			}, mc.cores=min(cores, length(wh.pa)))
+		names(deg.lst)<-pas;
+		deg=as.matrix(as.data.frame(deg.lst));
+	}
+	if(method=="deseq2"){
+		y=DESeqDataSetFromMatrix(countData =exp[, wh.ct], colData = data.frame(lab=rep("control",length(wh.ct))), design = ~ 1)
+		y <- estimateSizeFactors(y)
+		y <- estimateDispersions(y, quiet=T)
+		disp=dispersions(y)
+		mycutoff=rep(cutoff, length(disp));
+		mycutoff[disp > quantile(disp, probs=0.97)]=cutoff+0.05
+		rm(y)
+		deg.lst=mclapply(wh.pa, function(x){
+				y=DESeqDataSetFromMatrix(countData =exp[, c(wh.ct, x)], colData = data.frame(lab=c(rep("control",length(wh.ct)),"disease")), design = ~ 1)
+				y <- estimateSizeFactors(y);
+				y <- estimateDispersions(y, quiet=T)
+				z2=2**assay(varianceStabilizingTransformation(y));
+				p=pnbinom(z2[,n.ct+1], size=1/disp, mu=rowSums(z2[,1:n.ct])/n.ct, lower.tail = F)
+				bi=sapply(1:length(p), function(w){
+						if(p[w] <= mycutoff[w] )
+							return(1)
+						if(1- p[w] <= mycutoff[w] )
+							return(-1)
+						return(0);
+					});
+				return(bi);
+			}, mc.cores=min(cores, length(wh.pa)) )
+		names(deg.lst)<-pas
+		deg=as.matrix(as.data.frame(deg.lst))
+	}
+	if(method=="normalized"){
+		y=exp[,wh.ct];
+		mu=apply(y,1,mean);
+		sd=apply(y,1,sd);
+		rm(y);
+		deg.lst=mclapply(wh.pa, function(x){
+			w=exp[,x];
+			z=(w-mu)/sd;
+			p=pnorm(z,lower.tail=F);
+			bi=sapply(p, function(w){
+						if(w < cutoff )
+							return(1)
+						if(1-w < cutoff )
+							return(-1)
+						return(0);
+					});
+			return(bi);
+		} , mc.cores=min(cores, length(wh.pa)))
+		names(deg.lst)<-pas
+		deg=as.matrix(as.data.frame(deg.lst))
+	}
+	row.names(deg)<-genes;
+	colnames(deg)<-pas;
+	attr(deg, "class") <- "deg"
+	return(deg);
+}
